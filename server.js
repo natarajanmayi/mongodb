5e96b7686746eb1794e871d797b7c268a158af14
natarajanmayi <natarajanmayi@gmail.com>
2024-11-07 11:31:25 +0530

    Update server.js

--- server.js
+++ server.js
@@ -57,7 +57,7 @@ app.get("/api/posts/", async (req, res) => {
 });
 
 // Start the server
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
